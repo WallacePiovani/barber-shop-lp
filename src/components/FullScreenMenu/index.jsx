726b5ec9a781423fffbf0adf91e1f5994cf96425
WallacePiovani <whspiovani@gmail.com>
2023-09-30 22:45:15 -0300

    added hero section

--- src/components/FullScreenMenu/index.jsx
+++ src/components/FullScreenMenu/index.jsx
@@ -1,11 +1,8 @@
-//import { useState } from "react"
-
 const FullScreenMenu = () => {
-    //const [isOpen, setIsOpen] = useState (false)
 
   return (
-    <div className="fixed top-0 left-0 h-screen w-screen bg-preto z-30 bg-gradient-to-r  opacity-40">
-        <ul className="h-full flex flex-col justify-center items-center gap-20 font-openSans">
+    <div className="fixed top-0 left-0 h-screen w-screen bg-preto z-30 bg-gradient-to-r opacity-95">
+        <ul className="h-full flex flex-col justify-center items-center gap-20 font-openSans delay-300">
             <li><a className=" text-marrom hover:text-branco duration-300 cursor-pointer">Sobre Nos</a></li>
             <li><a className=" text-marrom hover:text-branco duration-300 cursor-pointer">Serviços</a></li>
             <li><a className=" text-marrom hover:text-branco duration-300 cursor-pointer">Barbeiros</a></li>
